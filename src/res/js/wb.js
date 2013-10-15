d4facb4d1522ed21d7c0e7676c155fc614a00217
astrider.com@gmail.com <astrider.com@gmail.com>
2013-10-16 04:23:59 +0900

    implemented panel snap and movement detection

--- src/res/js/wb.js
+++ src/res/js/wb.js
@@ -4,15 +4,27 @@ if (!window.WizardsBridge) WizardsBridge = {};
 	WizardsBridge.init = function() {
 		$(".panel").each(function() {
 			var id = $(this).attr("id").substr(-1);
-			var obstacles = new Array();
+					obstacles = [];
 			for (var i = 1; i <= 9; i++) {
 				if (i != id) obstacles.push("#panel" + i);
 			}
 
+			var tolerance = $(this).width()/3;
 			$(this).draggable({
 				obstacle: obstacles.toString(),
 				preventCollision: true,
-				containment: ".board"
+				containment: ".board",
+				snap: ".panel,.board",
+				snapTolerance: tolerance,
+				stop: function(event, ui) {
+					var leftMoved = ui.helper.width() * 2/3 < Math.abs(ui.position.left - ui.originalPosition.left),
+							topMoved = ui.helper.height() * 2/3 < Math.abs(ui.position.top - ui.originalPosition.top) ;
+					if (leftMoved || topMoved) {
+						console.log("moved");
+					} else {
+						console.log("not moved");
+					}
+				}
 			});
 		});
 	};
